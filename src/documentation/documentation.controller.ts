100936e7f3a7aacee1ac671aa5078f2bde6c0ac5
Alex Dcm <a.dcm33@gmail.com>
2023-05-18 18:33:30 +0200

    Added home controller, and edit/delete documentation

--- src/documentation/documentation.controller.ts
+++ src/documentation/documentation.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Param, Patch, Delete } from '@nestjs/common';
+import { endWith } from 'rxjs';
 import { DocumentationDto } from 'src/auth/dto';
 import { JwtGuard } from 'src/auth/guard';
 import { DocumentationService } from './documentation.service';
@@ -7,14 +8,35 @@ import { DocumentationService } from './documentation.service';
 export class DocumentationController {
     constructor(private documentationService: DocumentationService) {}
 
-    @Get(':id')
-    findOne(id: number) {
-        return this.documentationService.findOne(id);
+    @Get(':slug')
+    findOne(@Param('slug') slug: string) {
+        console.log("slug", slug);
+        
+        return this.documentationService.findOne(slug);
+    }
+
+    @Get('')
+    findAll() {
+        return this.documentationService.findAll();
     }
 
     @UseGuards(JwtGuard)
     @Post('new') 
     newDoc(@Body() dto: DocumentationDto) {
-        return this.documentationService.newDoc(dto);
+        return this.documentationService.new(dto);
+    }
+
+    @UseGuards(JwtGuard)
+    @Patch(':id') 
+    edit(@Param() params: { id: string }, @Body() dto: DocumentationDto) {  
+        // Todo : Make sure user is owner of documentation or admin      
+        return this.documentationService.edit(dto, Number(params.id));
+    }
+
+    @UseGuards(JwtGuard)
+    @Delete(':id')
+    deleteOne(@Param() params: { id: string }) {
+        // Todo : Make sure user is owner of documentation or admin
+        return this.documentationService.deleteOne(Number(params.id));
     }
 }
